dfd7fac5adc24f90758398e22a65ccea1b332015
Christopher Johnson <thechrisjohnson@protonmail.com>
2024-04-27 00:56:12 -0400

    Remove protocol stuff and use crate (#33)

--- src/common.rs
+++ src/common.rs
@@ -3,59 +3,7 @@
 use crate::error::SqrlError;
 use ed25519_dalek::VerifyingKey;
 use sha2::{Digest, Sha256};
-use std::fmt;
-use url::Url;
 use x25519_dalek::PublicKey;
-
-/// The general protocl for SQRL urls
-pub const SQRL_PROTOCOL: &str = "sqrl";
-
-/// Parses a SQRL url and breaks it into its parts
-#[derive(Debug, PartialEq)]
-pub struct SqrlUrl {
-    url: Url,
-}
-
-impl SqrlUrl {
-    /// Parse a SQRL url string and convert it into the object
-    pub fn parse(url: &str) -> Result<Self, SqrlError> {
-        let parsed = Url::parse(url)?;
-        if parsed.scheme() != SQRL_PROTOCOL {
-            return Err(SqrlError::new(format!(
-                "Invalid sqrl url, incorrect protocol: {}",
-                url
-            )));
-        }
-        if parsed.domain().is_none() {
-            return Err(SqrlError::new(format!(
-                "Invalid sqrl url, missing domain: {}",
-                url
-            )));
-        }
-
-        Ok(SqrlUrl { url: parsed })
-    }
-
-    /// Get the auth domain used for calculating identities
-    pub fn get_auth_domain(&self) -> String {
-        format!("{}{}", self.get_domain(), self.get_path())
-    }
-
-    fn get_domain(&self) -> String {
-        self.url.domain().unwrap().to_lowercase()
-    }
-
-    fn get_path(&self) -> String {
-        let path = self.url.path().strip_suffix('/').unwrap_or(self.url.path());
-        path.to_owned()
-    }
-}
-
-impl fmt::Display for SqrlUrl {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.url)
-    }
-}
 
 /// THe keys needed to unlock a SQRL identity
 pub struct IdentityUnlockKeys {
@@ -88,19 +36,6 @@ pub(crate) fn slice_to_u8_32(slice: &[u8]) -> Result<[u8; 32], SqrlError> {
     Ok(result)
 }
 
-pub(crate) fn slice_to_u8_64(slice: &[u8]) -> Result<[u8; 64], SqrlError> {
-    let mut result = [0; 64];
-    if slice.len() != 64 {
-        return Err(SqrlError::new(format!(
-            "Error converting vec<u8> to [u8; 64]: Expected 64 bytes, but found {}",
-            slice.len()
-        )));
-    }
-
-    result[..64].copy_from_slice(&slice[..64]);
-    Ok(result)
-}
-
 pub(crate) fn en_hash(input: &[u8]) -> [u8; 32] {
     let mut hasher = Sha256::new();
     hasher.update(input);
@@ -118,52 +53,5 @@ pub(crate) fn en_hash(input: &[u8]) -> [u8; 32] {
 pub(crate) fn xor(output: &mut [u8], other: &[u8]) {
     for i in 0..output.len() {
         output[i] ^= other[i];
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn sqrl_url_parse() {
-        let url = SqrlUrl::parse("sqrl://example.com?nut=8675309").unwrap();
-        assert_eq!(url.get_auth_domain(), "example.com");
-    }
-
-    #[test]
-    fn sqrl_url_parse_auth_domain_casing() {
-        let url = SqrlUrl::parse("sqrl://ExAmpLe.coM?nut=8675309").unwrap();
-        assert_eq!(url.get_auth_domain(), "example.com");
-    }
-
-    #[test]
-    fn sqrl_url_parse_auth_domain_trailing_forward_slash() {
-        let url = SqrlUrl::parse("sqrl://example.com/?nut=8675309").unwrap();
-        assert_eq!(url.get_auth_domain(), "example.com");
-    }
-
-    #[test]
-    fn sqrl_url_parse_auth_domain_port() {
-        let url = SqrlUrl::parse("sqrl://example.com:1234?nut=8675309").unwrap();
-        assert_eq!(url.get_auth_domain(), "example.com");
-    }
-
-    #[test]
-    fn sqrl_url_parse_auth_domain_password() {
-        let url = SqrlUrl::parse("sqrl://jonny:secret@example.com?nut=8675309").unwrap();
-        assert_eq!(url.get_auth_domain(), "example.com");
-    }
-
-    #[test]
-    fn sqrl_url_parse_auth_domain_path() {
-        let url = SqrlUrl::parse("sqrl://example.com/jimbo/?nut=8675309").unwrap();
-        assert_eq!(url.get_auth_domain(), "example.com/jimbo");
-    }
-
-    #[test]
-    fn sqrl_url_parse_auth_domain_path_case() {
-        let url = SqrlUrl::parse("sqrl://example.com/JIMBO?nut=8675309").unwrap();
-        assert_eq!(url.get_auth_domain(), "example.com/JIMBO");
     }
 }
